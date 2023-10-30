6f0d963bcaa77f6003e62e04d0c4e91f9b0e3d10
Ubuntu <ubuntu@ip-172-31-1-101.ec2.internal>
2023-10-30 06:13:31 +0000

    ユーザ登録機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,26 @@
 class UsersController < ApplicationController
   def index
-    #render "index"
+    @users = User.all
+  end
+  
+  def new
   end
   
   def create
-    hashed_password = BCrypt::Password.create(params[:pass])
-    user = User.new(uid: params[:uid], pass: hashed_password)
-    user.save
-    redirect_to users_index_path
+    if User.exists?(uid: params[:uid])
+      flash[:alert] = '※そのユーザIDは使えんでぇ～'
+      redirect_to new_user_path
+    else
+      hashed_password = BCrypt::Password.create(params[:pass])
+      user = User.new(uid: params[:uid], pass: hashed_password)
+      user.save
+      redirect_to users_path
+    end
+  end
+  
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to users_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   resources :users
   root 'users#index'
-  get 'users/index'
-  get 'users/new'
-  post 'users/create'
 end
